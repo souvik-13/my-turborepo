e9be54ed6aaabca028789ad442019de4e4393938
Souvik <souvikkarmakar8642@gmail.com>
2024-06-12 14:15:29 +0530

    myterborepo

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,18 +1,20 @@
 "use client";
 import { Button } from "@repo/ui/components/ui/button";
 import { Label } from "@repo/ui/components/custom/label";
-import { useName, useBalance } from "@repo/store/hooks";
+import { useName } from "@repo/store/hooks";
+import { balanceAtom } from "@repo/store/atoms";
+import { useRecoilState } from "recoil";
 
 export default function Page() {
   const name = useName();
-  const balance = useBalance();
+  const [balance, setBalance] = useRecoilState(balanceAtom);
   return (
     <main className="absolute top-0 left-0 w-full h-full grid place-items-center">
       <h1 className="text-4xl font-bold">
         {name} : {balance}
       </h1>
       <Label />
-      <Button>Click me</Button>
+      <Button onClick={() => setBalance((prev) => prev + 1)}>Click me</Button>
     </main>
   );
 }
